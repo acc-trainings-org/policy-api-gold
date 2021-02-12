fad0adfa0590cf7223712db023cec7c6463d88a2
rahul0309 <rahul.a0309@gmail.com>
2021-02-11 19:16:49 -0500

    changed controller as per discussion

--- src/main/java/com/acc/training/policyapigold/service/PolicyService.java
+++ src/main/java/com/acc/training/policyapigold/service/PolicyService.java
@@ -1,8 +1,5 @@
 package com.acc.training.policyapigold.service;
 
-import java.util.Collections;
-import java.util.List;
-
 import com.acc.training.policyapigold.model.Policy;
 import com.acc.training.policyapigold.repository.IPolicyRepository;
 
@@ -19,10 +16,13 @@ public class PolicyService {
         return policyRepository.save(policy);
     }
 
-    public List<Policy> getPolicies() {
-        List<Policy> result = policyRepository.findAll();
-        if (result.isEmpty()) {
-            return Collections.emptyList();
+    public Policy getPolicy(String policyId) {
+        Policy result = policyRepository.findByPolicyId(policyId);
+
+        //TODO - add web-client to get customer details
+        
+        if (result == null) {
+            return null;
         }
         return result;
     }
