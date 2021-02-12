32caaf9c05f74f67fe71fbb0b50309005df592ad
sandeepp873 <69438787+sandeepp873@users.noreply.github.com>
2021-02-12 12:59:45 -0500

    Customer service call
    Customer service call

--- src/main/java/com/acc/training/policyapigold/service/PolicyService.java
+++ src/main/java/com/acc/training/policyapigold/service/PolicyService.java
@@ -2,17 +2,22 @@ package com.acc.training.policyapigold.service;
 
 import java.util.Optional;
 
+import com.acc.training.policyapigold.model.Customer;
 import com.acc.training.policyapigold.model.Policy;
 import com.acc.training.policyapigold.repository.IPolicyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PolicyService {
 
     @Autowired
     IPolicyRepository policyRepository;
+
+    @Autowired
+    WebClient customerWebClient;
 
     public Policy createPolicy(Policy policy) {
         return policyRepository.save(policy);
@@ -21,11 +26,17 @@ public class PolicyService {
     public Policy getPolicy(String policyId) {
         Policy result = policyRepository.findByPolicyId(policyId);
 
-        //TODO: add web-client code for microservice communication
-
         if (result == null) {
             return null;
         }
+
+        String customerId = result.getCustomer().getCustomerId();
+
+        Customer customer = customerWebClient.get().uri("/customer/"+customerId).retrieve().bodyToMono(Customer.class).block();
+        if (customer != null){
+            result.setCustomer(customer);
+        }
+
         return result;
     }
 
