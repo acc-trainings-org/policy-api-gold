d13a07d32951ffb4e20c54f02bfb22621c87bc0f
rahul0309 <rahul.a0309@gmail.com>
2021-04-26 22:38:42 -0400

    added requestMapping into path

--- src/main/java/com/acc/training/policyapigold/controller/PolicyController.java
+++ src/main/java/com/acc/training/policyapigold/controller/PolicyController.java
@@ -21,7 +21,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@RequestMapping("/policy")
+@RequestMapping("${my.path}")
 public class PolicyController {
 
     @Autowired
