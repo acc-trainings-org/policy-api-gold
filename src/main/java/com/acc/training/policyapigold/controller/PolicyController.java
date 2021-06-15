966ca8bfd316589e1a493a8413f9e542d693df9e
Vikas Saxena <vikas070785@hotmail.com>
2021-06-14 22:26:27 -0400

    Update PolicyController.java

--- src/main/java/com/acc/training/policyapigold/controller/PolicyController.java
+++ src/main/java/com/acc/training/policyapigold/controller/PolicyController.java
@@ -23,7 +23,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@RequestMapping("/policy")
+@RequestMapping("${my.path}")
 @Configuration
 public class PolicyController {
 
